93c202aa55e8b59b8ce94b82e9ba219b1502d80a
Hadi-Sabra <131852949+Hadi-Sabra@users.noreply.github.com>
2025-02-12 16:18:46 +0200

    *

--- Labs/obj/Debug/net8.0/Labs.AssemblyInfo.cs
+++ Labs/obj/Debug/net8.0/Labs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1cff8419e26f3ca7b88def3f2253a6f68662f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215a099c1237f35f32a13baa7ab647085815fb6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
